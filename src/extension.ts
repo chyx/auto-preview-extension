bd86608d73a541d085cd75ef0221d016e99c959b
chyx111@qq.com <chyx111@qq.com>
2021-05-05 12:36:30 +0800

    ...to: chyx-pc; 2021-05-05 12:36; M: src/extension.ts |Auto: chyx-pc; 2021-05-05 12:36; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -90,7 +90,7 @@ async function openWikiLinkOnTheNextEditorColumn() {
 			const notes = await vscode.commands.executeCommand<Note[]>('vscodeMarkdownNotes.notesForWikiLink', wikiLink);
 			if (notes) {
 				openInPreviewEditor(notes[0].fsPath, nextColumn, false);
-				vscode.commands.executeCommand('vscode.close')
+				vscode.commands.executeCommand('workbench.action.closeOtherEditors');
 			}
 		}
 	}
