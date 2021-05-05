059fd4a40802c70df78e131eb9fe8f8cd683a458
chyx111@qq.com <chyx111@qq.com>
2021-05-05 14:51:09 +0800

    ...to: chyx-pc; 2021-05-05 14:51; M: src/extension.ts |Auto: chyx-pc; 2021-05-05 14:51; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -113,6 +113,8 @@ async function openWikiLinkOnThePreviousEditorColumn() {
 				await openInPreviewEditor(notes[0].fsPath, previousColumn, false);
 				vscode.commands.executeCommand('workbench.action.closeOtherEditors');
 			}
+		} else {
+
 		}
 	}
 }
