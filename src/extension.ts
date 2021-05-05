f6fd47bd1aca7fd6b2f8a1716dcf847bbf4b9550
chyx111@qq.com <chyx111@qq.com>
2021-05-05 15:01:12 +0800

    ...to: chyx-pc; 2021-05-05 15:01; M: src/extension.ts |Auto: chyx-pc; 2021-05-05 15:01; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -118,10 +118,9 @@ async function openWikiLinkOnThePreviousEditorColumn() {
 				await openInPreviewEditor(notes[0].fsPath, previousColumn, false);
 			}
 		} else {
-			await openDocumentInEditor()
 			const editor = vscode.window.visibleTextEditors.find(editor => editor.viewColumn == previousColumn)
 			if (editor != undefined) {
-				window.showTextDocument(editor.document);
+				await openDocumentInEditor(editor.document, previousColumn, false);
 			}
 		}
 	}
