2756b81bdb6067910abe2503254fd0e332b17ec3
chyx111@qq.com <chyx111@qq.com>
2021-07-04 15:27:42 +0800

    ...o: chyx-pc; 2021-07-04 15:27; M: src/utils/init.ts |Auto: chyx-pc; 2021-07-04 15:27; M: src/utils/init.ts

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -8,7 +8,7 @@ class Init {
     const { commands } = vscode.extensions.getExtension("chyx111.autopreview")
       ?.packageJSON.contributes;
     if (commands) {
-      commands.forEach(({ command, title }) => {
+      commands.forEach(({ command, title }): void => {
         const _ = title;
         const commandName = _.last(command.split(".")) as string;
         const handler = MAP.get(commandName);
