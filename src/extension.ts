6f4aa8a180e20349d86477191e89c0c700f66f29
chyx111@qq.com <chyx111@qq.com>
2021-07-04 15:32:18 +0800

    ...to: chyx-pc; 2021-07-04 15:32; M: src/extension.ts |Auto: chyx-pc; 2021-07-04 15:32; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,192 +1,238 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { workspace, window, commands, ExtensionContext, TextDocument, TextDocumentShowOptions } from 'vscode';
-import Init from './utils/init';
+import {
+  workspace,
+  window,
+  commands,
+  ExtensionContext,
+  TextDocument,
+  TextDocumentShowOptions,
+} from "vscode";
+import Init from "./utils/init";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "autopreview" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autopreview" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "autopreview.helloWorld",
+    () => {
+      openWikiLinkOnTheNextEditorColumn();
+      // The code you place here will be executed every time your command is executed
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('autopreview.helloWorld', () => {
-		openWikiLinkOnTheNextEditorColumn();
-		// The code you place here will be executed every time your command is executed
-	});
+  let disposable2 = vscode.commands.registerCommand(
+    "autopreview.openWikiLinkInNextTab",
+    () => {
+      openWikiLinkOnTheNextEditorColumn();
+      // The code you place here will be executed every time your command is executed
+    }
+  );
 
-	let disposable2 = vscode.commands.registerCommand('autopreview.openWikiLinkInNextTab', () => {
-		openWikiLinkOnTheNextEditorColumn();
-		// The code you place here will be executed every time your command is executed
-	});
+  let disposable3 = vscode.commands.registerCommand(
+    "autopreview.openWikiLinkInPreviousTab",
+    () => {
+      openWikiLinkOnThePreviousEditorColumn();
+      // The code you place here will be executed every time your command is executed
+    }
+  );
 
-	let disposable3 = vscode.commands.registerCommand('autopreview.openWikiLinkInPreviousTab', () => {
-		openWikiLinkOnThePreviousEditorColumn();
-		// The code you place here will be executed every time your command is executed
-	});
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+  Init.commands(context);
 
-	Init.commands(context);
+  let alreadyOpenedFirstMarkdown = false;
+  let previousUri = "";
 
-	let alreadyOpenedFirstMarkdown = false;
-	let previousUri = '';
+  function openMarkdownPreviewSideBySide() {
+    commands.executeCommand(markdown_preview_command_id).then(
+      () => {},
+      (e) => console.error(e)
+    );
+  }
 
-	function openMarkdownPreviewSideBySide() {
-		commands.executeCommand(markdown_preview_command_id)
-			.then(() => { }, (e) => console.error(e));
-	}
+  // if (window.activeTextEditor) {
+  // 	previewFirstMarkdown();
+  // } else {
+  // 	vscode.window.onDidChangeActiveTextEditor(() => {
+  // 		previewFirstMarkdown();
+  // 	});
+  // }
 
-	// if (window.activeTextEditor) {
-	// 	previewFirstMarkdown();
-	// } else {
-	// 	vscode.window.onDidChangeActiveTextEditor(() => {
-	// 		previewFirstMarkdown();
-	// 	});
-	// }
-
-	// vscode.window.onDidChangeTextEditorSelection(event => {
-	vscode.workspace.onWillSaveTextDocument((event) => {
-		if (event && event.document && event.document.languageId === "markdown") {
-			if (window.activeTextEditor?.viewColumn === 1) {
-				showFirstWikiLink(window.activeTextEditor?.document, 2);
-			}
-			if (window.activeTextEditor?.viewColumn === 2) {
-				showFirstWikiLink(window.activeTextEditor?.document, 3);
-			}
-		}
-	});
+  // vscode.window.onDidChangeTextEditorSelection(event => {
+  vscode.workspace.onWillSaveTextDocument((event) => {
+    if (event && event.document && event.document.languageId === "markdown") {
+      if (window.activeTextEditor?.viewColumn === 1) {
+        showFirstWikiLink(window.activeTextEditor?.document, 2);
+      }
+      if (window.activeTextEditor?.viewColumn === 2) {
+        showFirstWikiLink(window.activeTextEditor?.document, 3);
+      }
+    }
+  });
 }
 
 async function showFirstWikiLink(document: TextDocument, viewColumn: number) {
-    const editor = window.activeTextEditor;
-	if (editor === undefined) {
-		return;
-	}
-	// vscode.window.showInformationMessage('Doc opened: ' + editor.document.fileName);
-    const fullText = await generateTocText(editor.document);
-	const re = /\[\[[\w-]+\]\]/;
-	const firstLink = fullText.match(re);
-	if (firstLink) {
-		const notes = await vscode.commands.executeCommand<Note[]>('vscodeMarkdownNotes.notesForWikiLink', firstLink[0]);
-		if (notes) {
-			// vscode.window.showInformationMessage('First Link: ' + notes[0].fsPath);
-			openInPreviewEditor(notes[0].fsPath, viewColumn, true);
-		}
-	}
+  const editor = window.activeTextEditor;
+  if (editor === undefined) {
+    return;
+  }
+  // vscode.window.showInformationMessage('Doc opened: ' + editor.document.fileName);
+  const fullText = await generateTocText(editor.document);
+  const re = /\[\[[\w-]+\]\]/;
+  const firstLink = fullText.match(re);
+  if (firstLink) {
+    const notes = await vscode.commands.executeCommand<Note[]>(
+      "vscodeMarkdownNotes.notesForWikiLink",
+      firstLink[0]
+    );
+    if (notes) {
+      // vscode.window.showInformationMessage('First Link: ' + notes[0].fsPath);
+      openInPreviewEditor(notes[0].fsPath, viewColumn, true);
+    }
+  }
 }
 
 async function openWikiLinkOnTheNextEditorColumn() {
-	const column = window.activeTextEditor?.viewColumn;
-	if (!column) {
-		window.showInformationMessage('Current editor not found.');
-		return;
-	}
-	const nextColumn = column + 1;
-	const document = window.activeTextEditor?.document;
-	const position = window.activeTextEditor?.selection.active;
-	if (document && position) {
-		const wikiLink = getCurrentWikiLink(document, position);
-		if (wikiLink) {
-			const notes = await vscode.commands.executeCommand<Note[]>('vscodeMarkdownNotes.notesForWikiLink', wikiLink);
-			if (notes) {
-				await openInPreviewEditor(notes[0].fsPath, nextColumn, false);
-				vscode.commands.executeCommand('workbench.action.closeOtherEditors');
-			}
-		} else {
-			const editor = vscode.window.visibleTextEditors.find(editor => editor.viewColumn == nextColumn);
-			if (editor != undefined) {
-				await openDocumentInEditor(editor.document, nextColumn, false);
-			}
-		}
-	}
+  const column = window.activeTextEditor?.viewColumn;
+  if (!column) {
+    window.showInformationMessage("Current editor not found.");
+    return;
+  }
+  const nextColumn = column + 1;
+  const document = window.activeTextEditor?.document;
+  const position = window.activeTextEditor?.selection.active;
+  if (document && position) {
+    const wikiLink = getCurrentWikiLink(document, position);
+    if (wikiLink) {
+      const notes = await vscode.commands.executeCommand<Note[]>(
+        "vscodeMarkdownNotes.notesForWikiLink",
+        wikiLink
+      );
+      if (notes) {
+        await openInPreviewEditor(notes[0].fsPath, nextColumn, false);
+        vscode.commands.executeCommand("workbench.action.closeOtherEditors");
+      }
+    } else {
+      const editor = vscode.window.visibleTextEditors.find(
+        (editor) => editor.viewColumn == nextColumn
+      );
+      if (editor != undefined) {
+        await openDocumentInEditor(editor.document, nextColumn, false);
+      }
+    }
+  }
 }
 
 async function openWikiLinkOnThePreviousEditorColumn() {
-	const column = window.activeTextEditor?.viewColumn;
-	if (!column) {
-		window.showInformationMessage('Current editor not found.');
-		return;
-	}
-	const previousColumn = vscode.ViewColumn.One == column ? vscode.ViewColumn.One : column - 1;
-	const document = window.activeTextEditor?.document;
-	const position = window.activeTextEditor?.selection.active;
-	if (document && position) {
-		const wikiLink = getCurrentWikiLink(document, position);
-		if (wikiLink) {
-			const notes = await vscode.commands.executeCommand<Note[]>('vscodeMarkdownNotes.notesForWikiLink', wikiLink);
-			if (notes) {
-				await openInPreviewEditor(notes[0].fsPath, previousColumn, false);
-			}
-		} else {
-			const editor = vscode.window.visibleTextEditors.find(editor => editor.viewColumn == previousColumn);
-			if (editor != undefined) {
-				await openDocumentInEditor(editor.document, previousColumn, false);
-			}
-		}
-	}
+  const column = window.activeTextEditor?.viewColumn;
+  if (!column) {
+    window.showInformationMessage("Current editor not found.");
+    return;
+  }
+  const previousColumn =
+    vscode.ViewColumn.One == column ? vscode.ViewColumn.One : column - 1;
+  const document = window.activeTextEditor?.document;
+  const position = window.activeTextEditor?.selection.active;
+  if (document && position) {
+    const wikiLink = getCurrentWikiLink(document, position);
+    if (wikiLink) {
+      const notes = await vscode.commands.executeCommand<Note[]>(
+        "vscodeMarkdownNotes.notesForWikiLink",
+        wikiLink
+      );
+      if (notes) {
+        await openInPreviewEditor(notes[0].fsPath, previousColumn, false);
+      }
+    } else {
+      const editor = vscode.window.visibleTextEditors.find(
+        (editor) => editor.viewColumn == previousColumn
+      );
+      if (editor != undefined) {
+        await openDocumentInEditor(editor.document, previousColumn, false);
+      }
+    }
+  }
 }
 
-async function openDocumentInEditor(document: vscode.TextDocument, viewColumn: number, preserveFocus: boolean) {
-	const previousColumn = window.activeTextEditor?.viewColumn;
-	const previousDocument = window.activeTextEditor?.document;
-	const column: TextDocumentShowOptions = {
-		viewColumn: viewColumn,
-		preserveFocus: preserveFocus
-	};
-	await window.showTextDocument(document, column);
+async function openDocumentInEditor(
+  document: vscode.TextDocument,
+  viewColumn: number,
+  preserveFocus: boolean
+) {
+  const previousColumn = window.activeTextEditor?.viewColumn;
+  const previousDocument = window.activeTextEditor?.document;
+  const column: TextDocumentShowOptions = {
+    viewColumn: viewColumn,
+    preserveFocus: preserveFocus,
+  };
+  await window.showTextDocument(document, column);
 }
 
-
-async function openInPreviewEditor(uri: string, viewColumn: number, preserveFocus: boolean) {
-	const document = await vscode.workspace.openTextDocument(vscode.Uri.file(uri));
-	await openDocumentInEditor(document, viewColumn, preserveFocus);
+async function openInPreviewEditor(
+  uri: string,
+  viewColumn: number,
+  preserveFocus: boolean
+) {
+  const document = await vscode.workspace.openTextDocument(
+    vscode.Uri.file(uri)
+  );
+  await openDocumentInEditor(document, viewColumn, preserveFocus);
 }
 
 async function generateTocText(doc: TextDocument): Promise<string> {
-	return doc.getText();
+  return doc.getText();
 }
 
-const RxWikiLink = '\\[\\[[^\\]]+\\]\\]'; // [[wiki-link-regex(|with potential pipe)?]] Note: "sep" will be replaced with pipedWikiLinksSeparator on compile
-function getCurrentWikiLink(document: vscode.TextDocument, position: vscode.Position) {
-	const range = document.getWordRangeAtPosition(position, new RegExp(RxWikiLink, 'gi'));
-	if (range) {
-		// Our rxWikiLink contains [[ and ]] chars
-		// but the replacement words do NOT.
-		// So, account for the (exactly) 2 [[  chars at beginning of the match
-		// since our replacement words do not contain [[ chars
-		let s = new vscode.Position(range.start.line, range.start.character + 2);
-		// And, account for the (exactly) 2 ]]  chars at beginning of the match
-		// since our replacement words do not contain ]] chars
-		let e = new vscode.Position(range.end.line, range.end.character - 2);
-		// keep the end
-		let r = new vscode.Range(s, e);
-		const ref = document.getText(r);
-		return ref;
-	} else {
-		return null;
-	}
+const RxWikiLink = "\\[\\[[^\\]]+\\]\\]"; // [[wiki-link-regex(|with potential pipe)?]] Note: "sep" will be replaced with pipedWikiLinksSeparator on compile
+function getCurrentWikiLink(
+  document: vscode.TextDocument,
+  position: vscode.Position
+) {
+  const range = document.getWordRangeAtPosition(
+    position,
+    new RegExp(RxWikiLink, "gi")
+  );
+  if (range) {
+    // Our rxWikiLink contains [[ and ]] chars
+    // but the replacement words do NOT.
+    // So, account for the (exactly) 2 [[  chars at beginning of the match
+    // since our replacement words do not contain [[ chars
+    let s = new vscode.Position(range.start.line, range.start.character + 2);
+    // And, account for the (exactly) 2 ]]  chars at beginning of the match
+    // since our replacement words do not contain ]] chars
+    let e = new vscode.Position(range.end.line, range.end.character - 2);
+    // keep the end
+    let r = new vscode.Range(s, e);
+    const ref = document.getText(r);
+    return ref;
+  } else {
+    return null;
+  }
 }
 
-
-const close_other_editor_command_id = "workbench.action.closeEditorsInOtherGroups";
+const close_other_editor_command_id =
+  "workbench.action.closeEditorsInOtherGroups";
 const markdown_preview_command_id = "markdown.showPreviewToSide";
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 interface Note {
-	fsPath: string;
+  fsPath: string;
 }
 
 interface Editor {
-	column: string;
-	name: string;
+  column: string;
+  name: string;
 }
