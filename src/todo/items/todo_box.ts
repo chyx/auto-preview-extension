5f1b7739e4c187a87923b52ec2d390922224071a
chyx111@qq.com <chyx111@qq.com>
2021-07-04 13:18:06 +0800

    Auto: chyx-pc; 2021-07-04 13:18; M: |Auto: chyx-pc; 2021-07-04 13:18; M:

---
+++ src/todo/items/todo_box.ts
@@ -0,0 +1,13 @@
+import Consts from "../../consts";
+import Item from "./item";
+import Todo from "./todo";
+
+/* TODO BOX */
+class TodoBox extends Todo {
+  static is(str: string) {
+    return Item.is(str, Consts.regexes.todoBox);
+  }
+}
+
+/* EXPORT */
+export default TodoBox;
