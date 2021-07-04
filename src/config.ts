c122ab89adc703fa37335f7fdf8f093b8e33b0b7
chyx111@qq.com <chyx111@qq.com>
2021-07-04 15:38:00 +0800

    Auto: chyx-pc; 2021-07-04 15:38; M: src/config.ts |Auto: chyx-pc; 2021-07-04 15:38; M: src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,34 +11,34 @@ class Config {
     return _.get(Config.get(), key) as any;
   }
 
-  static check(config: any) {
-    // Check if the configuration is valid
+  // static check(config: any) {
+  //   // Check if the configuration is valid
 
-    const checkers = [
-      (config) =>
-        _.isString(_.get(config, "file")) &&
-        'Todo+: "todo.file" has been renamed to "todo.file.name"',
-      (config) =>
-        _.isString(_.get(config, "defaultContent")) &&
-        'Todo+: "todo.defaultContent" has been renamed to "todo.file.defaultContent"',
-      (config) =>
-        _.isArray(_.get(config, "tags.backgroundColors")) &&
-        'Todo+: "todo.tags.backgroundColors" has been renamed to "todo.colors.tags.background"',
-      (config) =>
-        _.isArray(_.get(config, "tags.foregroundColors")) &&
-        'Todo+: "todo.tags.foregroundColors" has been renamed to "todo.colors.tags.foreground"',
-    ];
+  //   const checkers = [
+  //     (config) =>
+  //       _.isString(_.get(config, "file")) &&
+  //       'Todo+: "todo.file" has been renamed to "todo.file.name"',
+  //     (config) =>
+  //       _.isString(_.get(config, "defaultContent")) &&
+  //       'Todo+: "todo.defaultContent" has been renamed to "todo.file.defaultContent"',
+  //     (config) =>
+  //       _.isArray(_.get(config, "tags.backgroundColors")) &&
+  //       'Todo+: "todo.tags.backgroundColors" has been renamed to "todo.colors.tags.background"',
+  //     (config) =>
+  //       _.isArray(_.get(config, "tags.foregroundColors")) &&
+  //       'Todo+: "todo.tags.foregroundColors" has been renamed to "todo.colors.tags.foreground"',
+  //   ];
 
-    const errors = _.compact(checkers.map((checker) => checker(config)));
+  //   const errors = _.compact(checkers.map((checker) => checker(config)));
 
-    if (!errors.length) {
-      return;
-    }
+  //   if (!errors.length) {
+  //     return;
+  //   }
 
-    errors.forEach((err) => vscode.window.showErrorMessage(err));
+  //   errors.forEach((err) => vscode.window.showErrorMessage(err));
 
-    throw new Error("Invalid configuration, check the changelog");
-  }
+  //   throw new Error("Invalid configuration, check the changelog");
+  // }
 }
 
 /* EXPORT */
