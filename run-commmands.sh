017842495a399a317b01a625193b6dd9a94a1049
chyx111@qq.com <chyx111@qq.com>
2021-07-04 12:11:04 +0800

    ...to: chyx-pc; 2021-07-04 12:11; M: run-commmands.sh |Auto: chyx-pc; 2021-07-04 12:11; M: run-commmands.sh

--- run-commmands.sh
+++ run-commmands.sh
@@ -5,6 +5,8 @@ npm install --save moment
 npm install --save to-time
 npm install --save @types/lodash
 npm install --save @types/diff
+npm install --save @types/moment
+npm install --save @types/to-time
 
 # Publish extensions.
 vsce package
