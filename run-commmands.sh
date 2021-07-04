b9ad90e39176c2828863fe992e934b8c61e298ce
chyx111@qq.com <chyx111@qq.com>
2021-07-04 13:17:45 +0800

    ...to: chyx-pc; 2021-07-04 13:17; M: run-commmands.sh |Auto: chyx-pc; 2021-07-04 13:17; M: run-commmands.sh

--- run-commmands.sh
+++ run-commmands.sh
@@ -3,10 +3,12 @@ npm install --save lodash
 npm install --save diff
 npm install --save moment
 npm install --save to-time
+npm install --save string-matches
 npm install --save @types/lodash
 npm install --save @types/diff
 npm install --save @types/moment
 npm install --save @types/moment-precise-range-plugin
+npm install --save-dev @types/string-matches
 
 # Publish extensions.
 vsce package
