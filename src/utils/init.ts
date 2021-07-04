e3045442ee0ea18be69b424a41577c6f62013e8d
chyx111@qq.com <chyx111@qq.com>
2021-07-04 15:27:11 +0800

    ...o: chyx-pc; 2021-07-04 15:27; M: src/utils/init.ts |Auto: chyx-pc; 2021-07-04 15:27; M: src/utils/init.ts

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -9,7 +9,7 @@ class Init {
       ?.packageJSON.contributes;
     if (commands) {
       commands.forEach(({ command, title }) => {
-        _ = title;
+        const _ = title;
         const commandName = _.last(command.split(".")) as string;
         const handler = MAP.get(commandName);
         if (handler !== undefined) {
