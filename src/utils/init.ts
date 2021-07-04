09fd654e6cc13af91ab1ce00cd9390704476530b
chyx111@qq.com <chyx111@qq.com>
2021-07-04 09:53:07 +0800

    Auto: chyx-pc; 2021-07-04 09:53; M: |Auto: chyx-pc; 2021-07-04 09:53; M:

---
+++ src/utils/init.ts
@@ -0,0 +1,27 @@
+import * as vscode from 'vscode';
+import * as _ from 'lodash';
+import * as Commands from '../commands';
+
+/* INIT */
+const Init = {
+
+  commands ( context: vscode.ExtensionContext ) {
+
+    const {commands} = vscode.extensions.getExtension ('chyx111.autopreview')?.packageJSON.contributes;
+    commands.forEach ( ({ command, title }) => {
+
+      const commandName = _.last ( command.split ( '.' ) ) as string,
+        handler = Commands[commandName],
+        disposable = vscode.commands.registerCommand ( command, handler );
+
+      context.subscriptions.push ( disposable );
+
+    });
+    return Commands;
+  },
+
+}
+
+
+/* EXPORT */
+export default Init;
