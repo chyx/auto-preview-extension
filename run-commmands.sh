fd8ca30ae0e010d93ce846b29b16549585cfb337
chyx111@qq.com <chyx111@qq.com>
2021-07-04 12:10:50 +0800

    ...M: package-lock.json,package.json,run-commmands.sh |Auto: chyx-pc; 2021-07-04 12:10; M: package-lock.json,package.json,run-commmands.sh

--- run-commmands.sh
+++ run-commmands.sh
@@ -1,6 +1,8 @@
 # Install packages
 npm install --save lodash
 npm install --save diff
+npm install --save moment
+npm install --save to-time
 npm install --save @types/lodash
 npm install --save @types/diff
 
