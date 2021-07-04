c01783640e013eef31fae66bb85a35a185803eaf
chyx111@qq.com <chyx111@qq.com>
2021-07-04 12:52:06 +0800

    ...chyx-pc; 2021-07-04 12:52; M: src/todo/document.ts |Auto: chyx-pc; 2021-07-04 12:52; M: src/todo/document.ts

--- src/todo/document.ts
+++ src/todo/document.ts
@@ -50,11 +50,11 @@ class Document {
   getItems(
     Item:
       | typeof Line
-      | typeof Todo
-      | typeof TodoBox
-      | typeof TodoFinished
-      | typeof TodoDone
-      | typeof TodoCancelled,
+    //   | typeof TodoBox
+    //   | typeof TodoFinished
+    //   | typeof TodoDone
+    //   | typeof TodoCancelled
+      | typeof Todo,
     regex: RegExp
   ) {
     const matchText = _.isString(this.text)
