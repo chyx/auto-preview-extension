0e78d8c8001a12b6521cc44c06873943d0a2b4c1
chyx111@qq.com <chyx111@qq.com>
2021-07-04 14:46:54 +0800

    ...o: chyx-pc; 2021-07-04 14:46; M: src/utils/init.ts |Auto: chyx-pc; 2021-07-04 14:46; M: src/utils/init.ts

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -1,27 +1,22 @@
-import * as vscode from 'vscode';
-import * as _ from 'lodash';
-import * as Commands from '../commands';
+import * as vscode from "vscode";
+import * as _ from "lodash";
+import * as Commands from "../commands";
 
 /* INIT */
 const Init = {
+  commands(context: vscode.ExtensionContext) {
+    const { commands } = vscode.extensions.getExtension("chyx111.autopreview")
+      ?.packageJSON.contributes;
+    commands.forEach(({ command, title }) => {
+      const commandName = _.last(command.split(".")) as string;
+      const handler = Commands.get(commandName);
+      const disposable = vscode.commands.registerCommand(command, handler);
 
-  commands ( context: vscode.ExtensionContext ) {
-
-    const {commands} = vscode.extensions.getExtension ('chyx111.autopreview')?.packageJSON.contributes;
-    commands.forEach ( ({ command, title }) => {
-
-      const commandName = _.last ( command.split ( '.' ) ) as string;
-        const handler = Commands.get(commandName);
-        const disposable = vscode.commands.registerCommand ( command, handler );
-
-      context.subscriptions.push ( disposable );
-
+      context.subscriptions.push(disposable);
     });
     return Commands;
   },
-
 };
-
 
 /* EXPORT */
 export default Init;
