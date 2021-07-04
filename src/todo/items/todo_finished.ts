9b41d2667faa4174d6e9fcab8a5eb53b216fd6cc
chyx111@qq.com <chyx111@qq.com>
2021-07-04 16:14:39 +0800

    Auto: chyx-pc; 2021-07-04 16:14; M: |Auto: chyx-pc; 2021-07-04 16:14; M:

---
+++ src/todo/items/todo_finished.ts
@@ -0,0 +1,14 @@
+import Consts from "../../consts";
+import Item from "./item";
+import Todo from "./todo";
+
+/* TODO FINISHED */
+
+class TodoFinished extends Todo {
+  static is(str: string) {
+    return Item.is(str, Consts.regexes.todoFinished);
+  }
+}
+
+/* EXPORT */
+export default TodoFinished;
