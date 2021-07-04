2ad64005d466ce57a2d481168685cafac9e65874
chyx111@qq.com <chyx111@qq.com>
2021-07-04 20:11:28 +0800

    ...; 2021-07-04 20:11; M: src/todo/decorators/line.ts |Auto: chyx-pc; 2021-07-04 20:11; M: src/todo/decorators/line.ts

--- src/todo/decorators/line.ts
+++ src/todo/decorators/line.ts
@@ -134,7 +134,7 @@ class Line {
   getDecorations(
     items: LineItem[],
     negRanges?: vscode.Range | vscode.Range[] | RegExp | RegExp[]
-  ): {type: TextEditorDecorationType, ranges: []} {
+  ): vscode.Range[] {type: TextEditorDecorationType, ranges: []} {
     let ranges = this.getItemsRanges(items, negRanges);
 
     return this.types.map((type, index) => ({
