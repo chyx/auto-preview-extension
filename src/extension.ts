1195e4053f0a0061238b2ff7604d1eb3b2b7cdfd
chyx111@qq.com <chyx111@qq.com>
2021-07-10 15:47:50 +0800

    ...to: chyx-pc; 2021-07-10 15:47; M: src/extension.ts |Auto: chyx-pc; 2021-07-10 15:47; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -174,7 +174,7 @@ async function openWikiLinkOnTheSpecifiedEditorColumn(direction: Direction) {
       }
       break;
     case Direction.down:
-      if (column + 1 <= window.visibleTextEditors.length) {
+      if (column + 2 <= window.visibleTextEditors.length) {
         nextColumn = column + 1;
       }
       break;
