2857f90127f60d6c1f63426cc8a4ef95e7a7e459
chyx111@qq.com <chyx111@qq.com>
2021-07-10 20:28:14 +0800

    ...2021-07-10 20:28; M: package.json,src/extension.ts |Auto: chyx-pc; 2021-07-10 20:28; M: package.json,src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -88,11 +88,11 @@ export function activate(context: vscode.ExtensionContext) {
       if (window.activeTextEditor?.viewColumn === 1) {
         showFirstWikiLink(window.activeTextEditor?.document, 2);
       }
-      if (window.activeTextEditor?.viewColumn === 2) {
-        showFirstWikiLink(window.activeTextEditor?.document, 3);
-      }
       if (window.activeTextEditor?.viewColumn === 3) {
         showFirstWikiLink(window.activeTextEditor?.document, 4);
+      }
+      if (window.activeTextEditor?.viewColumn === 5) {
+        showFirstWikiLink(window.activeTextEditor?.document, 6);
       }
     }
   });
