1c473974713c9cb22e978b454bab8f94dcccd584
chyx <chenyxun@gmail.com>
2021-07-16 08:54:28 +0000

    ...vultr.guest; 2021-07-16 16:54; M: src/extension.ts |Auto: vultr.guest; 2021-07-16 16:54; M: src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -140,7 +140,7 @@ async function openWikiLinkOnTheSpecifiedEditorColumn(direction: Direction) {
       }
       break;
     case Direction.down:
-      if (column + 2 <= window.visibleTextEditors.length) {
+      if (column + 1 <= window.visibleTextEditors.length) {
         nextColumn = column + 1;
       }
       break;
